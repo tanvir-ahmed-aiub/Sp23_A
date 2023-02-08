82f5bf4c5fded54e3536a12c75824fffde97c6cc
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2023-02-08 12:45:24 +0600

    form validation

---
+++ FormSubmission/FormSubmission/Controllers/RegController.cs
@@ -0,0 +1,27 @@
+﻿using FormSubmission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormSubmission.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration model) { 
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FormSubmission/FormSubmission/Models/Registration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "Please provide name")]
+        [StringLength(10)]
+        public string Name { get; set; }
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public string[] Hobbies { get; set; }
+        [Required]
+        public DateTime Dob { get; set; }
+    }
+}
